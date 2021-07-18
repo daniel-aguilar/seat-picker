3aa232315b76cc148298c0104df0655fbcc62f56
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2021-07-18 15:00:46 -0600

    Toggle cells when selected

--- src/grid.js
+++ src/grid.js
@@ -1,3 +1,4 @@
+import { pull } from 'lodash-es';
 import { Subject } from 'rxjs';
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
@@ -11,10 +12,64 @@ class Grid {
     return this._subject.asObservable();
   }
 
-  constructor(grid, table, subject) {
+  constructor(grid, table) {
     this._grid = grid;
     this._table = table;
-    this._subject = subject;
+
+    this._selectedCells = [];
+    this._subject = new Subject();
+  }
+
+  block(cellList) {
+    const cellCoordinates = cellList.map(c => this._decode(c));
+    cellCoordinates.forEach(c => {
+      const cell = this._grid[c.x][c.y];
+      cell.element.classList.add('taken');
+      cell.isAvailable = false;
+    });
+  }
+
+  toggle(td) {
+    const value = td.dataset.value;
+    const c = this._decode(value);
+    const cell = this._grid[c.x][c.y];
+    const previouslySelected = this._selectedCells.slice();
+
+    if (cell.isAvailable) {
+      if (cell.isSelected) {
+        pull(this._selectedCells, value);
+      }
+      else {
+        this._selectedCells.push(value);
+      }
+
+      cell.isSelected = !cell.isSelected;
+      cell.element.classList.toggle('selected');
+      this._subject.next(
+        JSON.stringify([previouslySelected, this._selectedCells])
+      );
+    }
+  }
+
+  _clearAll() {
+    this._grid.forEach(row =>
+      row.forEach(cell => {
+        cell.element.classList.remove('taken');
+        cell.isAvailable = true;
+      })
+    );
+  }
+
+  _decode(cellCoordinate) {
+    const x = cellCoordinate.substring(0, 1);
+    const y = cellCoordinate.substring(1, 2);
+
+    const coordinates = {
+      x: parseInt(x),
+      y: parseInt(y),
+    };
+
+    return coordinates;
   }
 }
 
@@ -23,6 +78,7 @@ export function makeGrid() {
   const table = document.createElement('table');
   const array = [];
   const subject = new Subject();
+  const grid = new Grid(array, table);
 
   const hrow = document.createElement('tr');
   addHiddenCorner();
@@ -30,7 +86,7 @@ export function makeGrid() {
   table.appendChild(hrow);
   addRows();
 
-  return new Grid(array, table, subject);
+  return grid;
 
   function addHiddenCorner() {
     const th = document.createElement('th');
@@ -58,8 +114,12 @@ export function makeGrid() {
         td.classList.add('cell');
         td.dataset.value = '' + i + j;
         td.addEventListener('click',
-            (e) => subject.next(e.currentTarget));
-        rows.push(td);
+            (e) => grid.toggle(e.currentTarget));
+        rows.push({
+          isAvailable: true,
+          isSelected: false,
+          element: td,
+        });
         row.appendChild(td);
       };
 
