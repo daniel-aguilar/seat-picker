b65c5113e73ca757dd35adef8c7e9963c4bcbe54
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2021-07-18 17:10:37 -0600

    Add player counter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import { makeGrid } from './grid';
+import { Client } from './client';
 
 const grid = makeGrid();
-const content = document.querySelector('.content');
+const content = document.querySelector('.grid');
+const playerCounter = document.querySelector('#player-count');
 content.appendChild(grid.element);
+
+const client = new Client();
+client.updates.subscribe(msg => {
+  playerCounter.innerHTML = msg.players;
+});
